54686ed75d7b01cd7d76f00962050bd1ca822460
Jappanjeet Singh <jappanjeet.99@gmail.com>
2020-05-13 14:50:22 +0530

    set debug value

--- blogging_web_app/settings.py
+++ blogging_web_app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages'
 
 ]
 
@@ -129,6 +130,12 @@ MEDIA_URL='/media/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL='blog-home'
 LOGIN_URL='login'
+AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE=False
+AWS_DEFAULT_ACL=None
+DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
 
